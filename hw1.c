ebcaee23654c7184c6a6c61b01a2e6c9c41e6e93
imDarkceus <69030573+imDarkceus@users.noreply.github.com>
2023-01-24 23:47:15 +0600

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main(){
+printf("Bangla \t\t A \t 4\n");
+printf("English \t A+ \t 5\n");
+printf("Mathematics \t A \t 4\n");
+printf("History \t A \t 4\n");
+printf("Economics \t A \t 4");
+
+}
