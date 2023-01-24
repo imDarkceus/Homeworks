c581870ae9a10713aacbe1ba45bbe7f35716c870
imDarkceus <69030573+imDarkceus@users.noreply.github.com>
2023-01-24 23:48:12 +0600

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(){
+printf("SL NO.\t\tItem\t\tQuantity\tPrice\t Total Cost\n");
+printf("------------------------------------------------------------------------\n");
+printf("1 \t Resistor(220 ohm)\t   5 \t\t1.20\t   6.00\n");
+printf("2 \t Resistor(10K ohm)\t   5 \t\t1.20\t   3.60\n");
+printf("3 \t Capacitor(2.2mF,15V)\t   2 \t\t5.50\t   11.00\n");
+printf("4 \t Battery(18650,Li-ion)\t   1 \t\t360\t   360\n");
+printf("5 \t wires\t                   -- \t\t20.00\t   20.00\n");
+}
